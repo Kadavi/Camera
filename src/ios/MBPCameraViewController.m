f91787a5c3694689e0a227396a5a37404b4f0f98
Grant Dawson <coedry@gmail.com>
2014-05-28 14:24:38 -0700

    Update MBPCameraViewController.m
    Crop marks and show loading animation.

--- src/ios/MBPCameraViewController.m
+++ src/ios/MBPCameraViewController.m
@@ -108,7 +108,7 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     _titlePanel = [[UILabel alloc] initWithFrame:CGRectZero];
     [_titlePanel setText:_title];
     [_titlePanel setFont:[UIFont boldSystemFontOfSize: kTitleFontSize]];
-    [_titlePanel setTextColor: [UIColor colorWithWhite: 0.0f alpha: 1.0f ]];
+    [_titlePanel setTextColor: [UIColor colorWithWhite: 1.0f alpha: 1.0f ]];
     [_titlePanel setTransform: CGAffineTransformMakeRotation(90 * M_PI / 180)];
     [_titlePanel setBackgroundColor: _framePanelColor];
     [overlay addSubview:_titlePanel];
@@ -119,8 +119,7 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     
     _captureButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [_captureButton setTitle:@"Take Picture" forState:UIControlStateNormal];
-    [_captureButton setBackgroundImage:[UIImage imageNamed:@"www/img/cancelup.png"] forState:UIControlStateNormal];
-    [_captureButton setBackgroundImage:[UIImage imageNamed:@"www/img/canceldown.png"] forState:UIControlStateHighlighted];
+
     [_captureButton addTarget:self action:@selector(takePictureWaitingForCameraToFocus) forControlEvents:UIControlEventTouchUpInside];
     
     [overlay addSubview:_captureButton];
@@ -131,6 +130,53 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     [_cancelButton addTarget:self action:@selector(cancelTakePicture) forControlEvents:UIControlEventTouchUpInside];
     
     [overlay addSubview:_cancelButton];
+    
+    // <-- Draw crop marks -->
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    NSInteger height = bounds.size.height;
+    NSInteger width = bounds.size.width - kHeaderHeightPhone;
+    
+	CGPoint topLeftPts = CGPointMake(kFrameBorderSizePhone, kFrameBorderSizePhone);
+	CGPoint topRightPts = CGPointMake(width - kFrameBorderSizePhone, kFrameBorderSizePhone);
+	CGPoint bottomLeftPts = CGPointMake(kFrameBorderSizePhone, height - kCaptureButtonHeightPhone - (kCaptureButtonVerticalInsetPhone * 2));
+	CGPoint bottomRightPts = CGPointMake(width - kFrameBorderSizePhone, height - kCaptureButtonHeightPhone - (kCaptureButtonVerticalInsetPhone * 2));
+    
+    CGMutablePathRef cropMarkPath = CGPathCreateMutable();
+    
+    CGFloat borderLineLength = 30;
+    
+    // Top left corner marks
+	CGPathMoveToPoint(cropMarkPath, NULL, topLeftPts.x, topLeftPts.y + borderLineLength);
+	CGPathAddLineToPoint(cropMarkPath, NULL, topLeftPts.x, topLeftPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, topLeftPts.x + borderLineLength, topLeftPts.y);
+    
+    // Top right corner marks
+    CGPathMoveToPoint(cropMarkPath, NULL, topRightPts.x - borderLineLength, topRightPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, topRightPts.x, topRightPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, topRightPts.x, topRightPts.y + borderLineLength);
+    
+    // Bottom right mark
+	CGPathMoveToPoint(cropMarkPath, NULL, bottomRightPts.x, bottomRightPts.y - borderLineLength);
+	CGPathAddLineToPoint(cropMarkPath, NULL, bottomRightPts.x, bottomRightPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, bottomRightPts.x - borderLineLength, bottomRightPts.y);
+    
+    // Bottom left corner mark
+    CGPathMoveToPoint(cropMarkPath, NULL, bottomLeftPts.x + borderLineLength, bottomLeftPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, bottomLeftPts.x, bottomLeftPts.y);
+	CGPathAddLineToPoint(cropMarkPath, NULL, bottomLeftPts.x, bottomLeftPts.y - borderLineLength);
+	   
+    CAShapeLayer *cropMarkLayer = [CAShapeLayer layer];
+    [cropMarkLayer setBounds:overlay.bounds];
+    [cropMarkLayer setPosition:overlay.center];
+    [cropMarkLayer setFillColor:[[UIColor clearColor] CGColor]];
+    [cropMarkLayer setStrokeColor:[[UIColor blackColor] CGColor]];
+    [cropMarkLayer setLineWidth:1.0f];
+    [cropMarkLayer setLineJoin:kCALineJoinBevel];
+    [cropMarkLayer setLineDashPattern:
+    [NSArray arrayWithObjects:[NSNumber numberWithInt:10], [NSNumber numberWithInt:5],nil]];
+    [cropMarkLayer setPath:cropMarkPath];
+    CGPathRelease(cropMarkPath);
+    [[overlay layer] addSublayer:cropMarkLayer];
     
     return overlay;
 }
@@ -169,14 +215,15 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     NSInteger logoSizeW = _logoPanel.image.size.width;
     NSInteger logoSizeH =_logoPanel.image.size.height;
     
+    CGFloat textPadding = 10;
     
-    _captureButton.frame = CGRectMake((((width) / 2) - (kCaptureButtonWidthPhone / 2)),
+    _captureButton.frame = CGRectMake((((width) / 2) - (kCaptureButtonWidthPhone / 2)) + textPadding,
                                       bounds.size.height - kCaptureButtonHeightPhone - kCaptureButtonVerticalInsetPhone,
                                       kCaptureButtonWidthPhone,
                                       kCaptureButtonHeightPhone);
     _captureButton.layer.cornerRadius = kCaptureButtonRadiusPhone;
     
-    _cancelButton.frame = CGRectMake(-20,
+    _cancelButton.frame = CGRectMake(-textPadding,
                                      bounds.size.height - kCaptureButtonHeightPhone - kCaptureButtonVerticalInsetPhone,
                                      kCaptureButtonWidthPhone - 5,
                                      kCaptureButtonHeightPhone);
@@ -208,7 +255,6 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     NSInteger logoSizeW = _logoPanel.image.size.width;
     NSInteger logoSizeH =_logoPanel.image.size.height;
     
-    
     _captureButton.frame = CGRectMake((((width) / 2) - (kCaptureButtonWidthPhone / 2)),
                                       bounds.size.height - kCaptureButtonHeightPhone - kCaptureButtonVerticalInsetPhone,
                                       kCaptureButtonWidthPhone,
@@ -237,6 +283,8 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     _titlePanel.frame = CGRectMake(width - kFrameBorderSizePhone, kFrameBorderSizePhone, kFrameBorderSizePhone, height -kFrameBorderSizePhone - _buttonPanel.frame.size.height);
     
     previewLayer.frame = CGRectMake(kFrameBorderSizePhone, kFrameBorderSizePhone, width - (kFrameBorderSizePhone * 2), height - kFrameBorderSizePhone - kCaptureButtonHeightPhone - (kCaptureButtonVerticalInsetPhone * 2));
+    
+    
 }
 
 - (void)layoutForPhoneWithTallScreen {
@@ -246,7 +294,6 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     NSInteger width = bounds.size.width - kHeaderHeightPhone;
     NSInteger logoSizeW = _logoPanel.image.size.width;
     NSInteger logoSizeH =_logoPanel.image.size.height;
-    
     
     _captureButton.frame = CGRectMake((((width) / 2) - (kCaptureButtonWidthPhone / 2)),
                                       bounds.size.height - kCaptureButtonHeightPhone - kCaptureButtonVerticalInsetPhone,
@@ -285,7 +332,6 @@ static const CGFloat kAspectRatio = 125.0f / 86;
     NSInteger width = bounds.size.width - kHeaderHeightPhone;
     NSInteger logoSizeW = _logoPanel.image.size.width;
     NSInteger logoSizeH =_logoPanel.image.size.height;
-    
     
     _captureButton.frame = CGRectMake((((width) / 2) - (kCaptureButtonWidthPhone / 2)),
                                       bounds.size.height - kCaptureButtonHeightPhone - kCaptureButtonVerticalInsetPhone,
@@ -381,6 +427,7 @@ static const CGFloat kAspectRatio = 125.0f / 86;
 }
 
 - (void)takePicture {
+    [_activityIndicator startAnimating];
     AVCaptureConnection *videoConnection = [self videoConnectionToOutput:_stillImageOutput];
     [_stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageSampleBuffer, NSError *error) {
         NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];
